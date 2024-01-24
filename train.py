a09f2576ed7e0ec9b59154a505d87ec783129d68
xtliu97 <mielionsk@oulook.com>
2024-01-24 19:58:24 +0800

    add feature extraction

--- train.py
+++ train.py
@@ -1,14 +1,21 @@
 import argparse
 
+import pytorch_lightning as pl
+
 from voca.config import Config
 from voca.model import VocaModel
-from voca.daatset import VocaDataLoader
+from voca.daatset import VocaDataset
 
 
 def main():
     config = Config().parse_from_file()
-    dataloader = VocaDataLoader(config)
+    dataset = VocaDataset(config)
+    train_dataloader = dataset.train_dataloader()
+    val_dataloader = dataset.val_dataloader()
+    
     model = VocaModel(config)
     
+    trainer = pl.Trainer(    )
     
+    trainer.fit(model, train_dataloader, val_dataloader)
     
