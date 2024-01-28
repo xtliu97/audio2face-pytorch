8ee9d4d9a31c0d5557568af7ffbf3e575e348ce5
xtliu97 <xtliu1112@outlook.com>
2024-01-28 23:05:38 +0800

    add FaceMesh and Renderer

--- utils/convert_ply.py
+++ utils/convert_ply.py
@@ -4,13 +4,19 @@ import os
 import trimesh
 from psbody.mesh import Mesh
 
-def convert_ply_to_obj(ply_fanme:str) -> None:
+
+def convert_ply_to_obj(ply_fanme: str) -> None:
     base_dir = os.path.dirname(ply_fanme)
-    obj_fname = os.path.join(base_dir, os.path.basename(ply_fanme).replace(".ply", ".obj"))
+    obj_fname = os.path.join(
+        base_dir, os.path.basename(ply_fanme).replace(".ply", ".obj")
+    )
     mesh = Mesh(filename=ply_fanme)
-    print(f"psbody.mesh.Mesh loaded from {ply_fanme}, vshape:{mesh.v.shape}, fshape:{mesh.f.shape}")
+    print(
+        f"psbody.mesh.Mesh loaded from {ply_fanme}, vshape:{mesh.v.shape}, fshape:{mesh.f.shape}"
+    )
     print("Converting to trimesh.Trimesh...")
     trimesh.Trimesh(vertices=mesh.v, faces=mesh.f).export(obj_fname)
     print("Saved to {}".format(obj_fname))
-    
+
+
 convert_ply_to_obj("../template/FLAME_sample.ply")
--- utils/facemesh.py
+++ utils/facemesh.py
@@ -1,32 +1,36 @@
+import os
+
 import numpy as np
 import trimesh
+
 
 class FaceMesh:
     def __check_mesh_validity(self, arr):
         assert arr.ndim == 2, "arr.ndim must be 2"
         assert arr.shape[1] == 3, "arr.shape[1] must be 3"
-        
+
     def __init__(self, verts, faces):
         self.__check_mesh_validity(verts)
         self.__check_mesh_validity(faces)
-        
+
         self._verts = np.array(verts)
         self._faces = np.array(faces)
-        
-    @property   
+
+    @property
     def verts(self):
         return self._verts
-    
+
     def set_verts(self, verts) -> None:
         self.__check_mesh_validity(verts)
         self._verts = np.array(verts)
-      
+
     @property
     def faces(self):
         return self._faces
-    
+
     @classmethod
-    def load(cls, fname:str) -> 'FaceMesh':
+    def load(cls, fname: str) -> "FaceMesh":
+        assert os.path.exists(fname), f"{fname} does not exist"
         if fname.endswith(".obj"):
             trimesh_mesh = trimesh.load(fname)
             return cls(trimesh_mesh.vertices, trimesh_mesh.faces)
@@ -34,13 +38,15 @@ class FaceMesh:
             try:
                 from psbody.mesh import Mesh
             except ImportError:
-                raise ImportError("psbody.mesh is required to load .ply file, \
-if you don't have psbody.mesh, please use convert_ply_to_obj() in utils/convert_ply.py to convert .ply to .obj")
+                raise ImportError(
+                    "psbody.mesh is required to load .ply file, \
+if you don't have psbody.mesh, please use convert_ply_to_obj() in utils/convert_ply.py to convert .ply to .obj"
+                )
             mesh = Mesh(filename=fname)
             return cls(mesh.v, mesh.f)
-    
+
     def copy(self):
         return FaceMesh(self.verts.copy(), self.faces.copy())
-    
+
     def trimesh(self):
         return trimesh.Trimesh(vertices=self.verts, faces=self.faces)
