7d32cf1dbe6aea9188b2e46099f6164e7b92a0e8
xtliu97 <xtliu1112@outlook.com>
2024-02-18 23:06:10 +0800

    add model

--- render.py
+++ render.py
@@ -16,80 +16,83 @@ verts = np.load("verts_sample.npy")
 center = np.mean(verts[0], axis=0)
 last_frame = verts[0]
 
+
 def render(index):
     global verts, center, last_frame
     verts_sample = verts[index]
     texture_mesh = FaceMesh.load("FLAME_sample.obj")
     # rotate verts_sample
     rot = np.zeros(3)
-    verts_sample[:] = cv2.Rodrigues(rot)[0].dot((verts_sample-center).T).T+center
+    verts_sample[:] = cv2.Rodrigues(rot)[0].dot((verts_sample - center).T).T + center
 
     texture_mesh = texture_mesh.copy()
     texture_mesh.set_verts(verts_sample)
 
     # render
     render_mesh = pyrender.Mesh.from_trimesh(texture_mesh.trimesh(), smooth=True)
-    
-    
-    camera_params = {'c': np.array([400, 400]),
-                     'k': np.array([-0.19816071, 0.92822711, 0, 0, 0]),
-                     'f': np.array([4754.97941935 / 2, 4754.97941935 / 2])}
 
-    frustum = {'near': 0.01, 'far': 3.0, 'height': 800, 'width': 800}
-    
+    camera_params = {
+        "c": np.array([400, 400]),
+        "k": np.array([-0.19816071, 0.92822711, 0, 0, 0]),
+        "f": np.array([4754.97941935 / 2, 4754.97941935 / 2]),
+    }
+
+    frustum = {"near": 0.01, "far": 3.0, "height": 800, "width": 800}
+
     z_offset = 0.0
     intensity = 1.5
-    
-    scene = pyrender.Scene(ambient_light=[.2, .2, .2], bg_color=[255, 255, 255])
-    camera = pyrender.IntrinsicsCamera(fx=camera_params['f'][0],
-                                      fy=camera_params['f'][1],
-                                      cx=camera_params['c'][0],
-                                      cy=camera_params['c'][1],
-                                      znear=frustum['near'],
-                                      zfar=frustum['far'])
+
+    scene = pyrender.Scene(ambient_light=[0.2, 0.2, 0.2], bg_color=[255, 255, 255])
+    camera = pyrender.IntrinsicsCamera(
+        fx=camera_params["f"][0],
+        fy=camera_params["f"][1],
+        cx=camera_params["c"][0],
+        cy=camera_params["c"][1],
+        znear=frustum["near"],
+        zfar=frustum["far"],
+    )
 
     scene.add(render_mesh, pose=np.eye(4))
 
     camera_pose = np.eye(4)
-    camera_pose[:3,3] = np.array([0, 0, 1.0-z_offset])
-    scene.add(camera, pose=[[1, 0, 0, 0],
-                            [0, 1, 0, 0],
-                            [0, 0, 1, 1],
-                            [0, 0, 0, 1]])
+    camera_pose[:3, 3] = np.array([0, 0, 1.0 - z_offset])
+    scene.add(camera, pose=[[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]])
 
     angle = np.pi / 6.0
-    pos = camera_pose[:3,3]
-    light_color = np.array([1., 1., 1.])
+    pos = camera_pose[:3, 3]
+    light_color = np.array([1.0, 1.0, 1.0])
     light = pyrender.PointLight(color=light_color, intensity=intensity)
 
     light_pose = np.eye(4)
-    light_pose[:3,3] = pos
+    light_pose[:3, 3] = pos
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([angle, 0, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([angle, 0, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] =  cv2.Rodrigues(np.array([-angle, 0, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([-angle, 0, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([0, -angle, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([0, -angle, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
-    light_pose[:3,3] = cv2.Rodrigues(np.array([0, angle, 0]))[0].dot(pos)
+    light_pose[:3, 3] = cv2.Rodrigues(np.array([0, angle, 0]))[0].dot(pos)
     scene.add(light, pose=light_pose.copy())
 
     flags = pyrender.RenderFlags.SKIP_CULL_FACES
-    
+
     try:
-        r = pyrender.OffscreenRenderer(viewport_width=frustum['width'], viewport_height=frustum['height'])
+        r = pyrender.OffscreenRenderer(
+            viewport_width=frustum["width"], viewport_height=frustum["height"]
+        )
         color, depth = r.render(scene, flags=flags)
         last_frame = color
     except Exception as e:
-        print('pyrender: Failed rendering frame' + str(e) )
+        print("pyrender: Failed rendering frame" + str(e))
         color = last_frame
-    color =  color[..., ::-1]
+    color = color[..., ::-1]
     cv2.imwrite(f"output/render_{index}.png", color)
-    
+
 
 if __name__ == "__main__":
     n = verts.shape[0]
@@ -100,13 +103,23 @@ if __name__ == "__main__":
     tic = time.time()
     for i in progress.track(range(n)):
         render(i)
-    
+
     print(f"Rendered {n} images in {time.time() - tic} seconds")
-        
-        
-        
-        
-       
-    call(["ffmpeg", "-framerate", "60", "-i", f"output/render_%d.png", "-c:v", "libx264", "-r", "30", "-pix_fmt", "yuv420p", "output.mp4"])
+
+    call(
+        [
+            "ffmpeg",
+            "-framerate",
+            "60",
+            "-i",
+            f"output/render_%d.png",
+            "-c:v",
+            "libx264",
+            "-r",
+            "30",
+            "-pix_fmt",
+            "yuv420p",
+            "output.mp4",
+        ]
+    )
     # call(["rm", f"output/render_{i}.png"])
-        
--- train.py
+++ train.py
@@ -1,21 +1,170 @@
-import argparse
+# %% import
+import os
 
-import pytorch_lightning as pl
+import numpy as np
+from rich.progress import track
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import tensorboardX as tb
 
-from voca.config import Config
-from voca.model import VocaModel
-from voca.daatset import VocaDataset
+# import pytorch_lightning as pl
+
+from dataset.vocaset import VocaSet
+from model.audio2bs import Audio2Face
+from utils.renderer import Renderer, FaceMesh, images_to_video
+
+# %% load dataset
+dataset_path = os.getcwd()
+trainset = VocaSet(dataset_path, "train")
+valset = VocaSet(dataset_path, "val")
+print(f"Trainset size: {len(trainset)}")
+print(f"Valset size: {len(valset)}")
+verts = np.load("assets/verts_sample.npy")
+sample_verts = np.mean(verts, axis=0)
+print(sample_verts.shape)
 
 
-def main():
-    config = Config().parse_from_file()
-    dataset = VocaDataset(config)
-    train_dataloader = dataset.train_dataloader()
-    val_dataloader = dataset.val_dataloader()
-    
-    model = VocaModel(config)
-    
-    trainer = pl.Trainer(    )
-    
-    trainer.fit(model, train_dataloader, val_dataloader)
-    
+# %% load model
+def collate_fn(batch):
+    inputs = []
+    labels = []
+    for sample in batch:
+        inputs.append(sample.feature[:, :64].transpose(0, 1))
+        labels.append(
+            torch.tensor(sample.verts).reshape(-1).float()
+            - torch.tensor(sample_verts).reshape(-1).float()
+        )
+    inputs = torch.stack(inputs)
+    labels = torch.stack(labels)
+    return inputs, labels
+
+
+class Loss:
+    def __call__(self, pred, gt):
+        loss_p = torch.mean((pred - gt) ** 2)
+        split_pred = torch.split(pred, 2, dim=0)
+        split_gt = torch.split(gt, 2, dim=0)
+        loss_m = 2 * torch.mean(
+            (split_pred[0] - split_pred[1] - (split_gt[0] - split_gt[1])) ** 2
+        )
+        return loss_p + loss_m
+
+
+# %% trainer
+model = Audio2Face(5023 * 3)
+
+
+class Trainer:
+    def __init__(self, batch_size=24, lr=1e-3):
+        self.model = model
+        self.dataset_path = os.getcwd()
+        self.trainset = VocaSet(self.dataset_path, "train")
+        self.valset = VocaSet(self.dataset_path, "val")
+        self.batch_size = batch_size
+        self.lr = lr
+        self.device = torch.device("mps")
+        self.model.to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.criterion = Loss()
+        self.train_loader = DataLoader(
+            self.trainset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            collate_fn=collate_fn,
+            pin_memory=True,
+        )
+        self.val_loader = DataLoader(
+            self.valset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            collate_fn=collate_fn,
+            pin_memory=True,
+        )
+        self.logger = tb.SummaryWriter("logs")
+
+    def train(self, epochs=1):
+        for epoch in range(epochs):
+            self.model.train()
+            running_loss = 0.0
+            for i, data in enumerate(self.train_loader):
+                inputs, labels = data
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+                running_loss += loss.item()
+                if i % 100 == 99:
+                    self.logger.add_scalar(
+                        "train_loss",
+                        running_loss / 100,
+                        epoch * len(self.train_loader) + i,
+                    )
+                    print(
+                        f"Epoch {epoch}/{epochs} iter {i}/{len(self.train_loader)} loss: {running_loss / 100}"
+                    )
+                    running_loss = 0.0
+            self.validate(epoch)
+            # save model
+            torch.save(self.model.state_dict(), f"model_{epoch}.pth")
+        print("Finished Training")
+
+    def validate(self, epoch):
+        self.model.eval()
+        running_loss = 0.0
+        with torch.no_grad():
+            for i, data in enumerate(self.val_loader, 0):
+                inputs, labels = data
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                running_loss += loss.item()
+            self.logger.add_scalar(
+                "val_loss", running_loss / len(self.val_loader), epoch
+            )
+            print(f"Epoch {epoch} val_loss: {running_loss / len(self.val_loader)}")
+
+
+trainer = Trainer()
+trainer.train()
+# %%
+
+
+def inference():
+    texture_mesh = FaceMesh.load("assets/FLAME_sample.obj")
+    renderer = Renderer(texture_mesh)
+
+    model = Audio2Face(5023 * 3)
+    model.load_state_dict(torch.load("model_0.pth"))
+    model.eval()
+    dataset_path = os.getcwd()
+    valset = VocaSet(dataset_path, "val")
+    val_loader = DataLoader(
+        valset, batch_size=1, shuffle=False, collate_fn=collate_fn, pin_memory=True
+    )
+    device = torch.device("cpu")
+    model.to(device)
+    gts = []
+    preds = []
+    with torch.no_grad():
+        for i, data in track(enumerate(val_loader), total=len(val_loader)):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            gts.append(labels.reshape(-1, 5023, 3).numpy() + sample_verts)
+            preds.append(outputs.reshape(-1, 5023, 3).numpy() + sample_verts)
+            if i > 200:
+                break
+    gts = np.concatenate(gts)
+    preds = np.concatenate(preds)
+    gts = renderer.render(gts)
+    preds = renderer.render(preds)
+    images_to_video(gts, "gt.mp4")
+    images_to_video(preds, "pred.mp4")
+
+
+inference()
